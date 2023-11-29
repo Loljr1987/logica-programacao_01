c3f7d0e3a52dcc23013e5d688df6381b16d5740f
Pequeno <lucivaldojr@hotmail.com>
2023-11-28 21:10:30 -0300

    terminada a terceira aula

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 2;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+let numeroSecreto = 5;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-// se chute for igual ao número secreto
-if (chute == numeroSecreto) {
-     alert(`Isso ai! Você descobriu o número sercreto ${numeroSecreto}`);
-} else {
-     if (chute > numeroSecreto) {
-          alert(`o número secreto é menor que ${chute}`);
+// enquanto o número não for igual ao n.S. 
+while(chute != numeroSecreto ) {
+     chute = prompt('Escolha um número entre 1 e 10');
+     // se chute for igual ao número secreto
+     if (chute == numeroSecreto) {
+          alert(`Isso ai! Você descobriu o número sercreto ${numeroSecreto} com ${tentativas} tentativas.`);
      } else {
-          alert()
+          if (chute > numeroSecreto) {
+               alert(`o número secreto é menor que ${chute}`);
+          } else {
+               alert(`o número secreto é naior que ${chute}`);
+          }
+          // tentativas = tentativas + 1;
+          tentativas++
      }
 }
